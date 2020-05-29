d097ce5e88764136d30f464b9c32d217312a05b5
Mateusz Karłowski <mateusz.karlowski@zoho.com>
2020-05-29 11:38:34 +0200

    Updates

---
+++ precache-manifest.cb7ed2193bf067e0b3c8130b1a56b405.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a89d5e17c89bbcd8e912c7400748bb50",
+    "url": "/index.html"
+  },
+  {
+    "revision": "01f6e76451e6c94b4868",
+    "url": "/static/css/main.97e2a367.chunk.css"
+  },
+  {
+    "revision": "fab3f06fe65265fb48f2",
+    "url": "/static/js/2.b7697b78.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.b7697b78.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "01f6e76451e6c94b4868",
+    "url": "/static/js/main.28502d94.chunk.js"
+  },
+  {
+    "revision": "bf862a4daeed81f758e6",
+    "url": "/static/js/runtime-main.6180cfd4.js"
+  }
+]);
