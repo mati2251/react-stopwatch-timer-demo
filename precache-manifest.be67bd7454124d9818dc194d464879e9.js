e11cc149b830f12dbbeb7fa14ea6de2ce163efda
Mateusz Karłowski <mateusz.karlowski@zoho.com>
2020-05-29 11:59:33 +0200

    Updates

---
+++ precache-manifest.be67bd7454124d9818dc194d464879e9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "60c8ef57a1969cceec70a2e4f2b736ed",
+    "url": "/react-stopwatch-timer-demo/index.html"
+  },
+  {
+    "revision": "7b8b8d456490e6bc260f",
+    "url": "/react-stopwatch-timer-demo/static/css/main.97e2a367.chunk.css"
+  },
+  {
+    "revision": "fab3f06fe65265fb48f2",
+    "url": "/react-stopwatch-timer-demo/static/js/2.b7697b78.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-stopwatch-timer-demo/static/js/2.b7697b78.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7b8b8d456490e6bc260f",
+    "url": "/react-stopwatch-timer-demo/static/js/main.9038a488.chunk.js"
+  },
+  {
+    "revision": "7d99a47eab944f6ad4fc",
+    "url": "/react-stopwatch-timer-demo/static/js/runtime-main.cddf32a2.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.cb7ed2193bf067e0b3c8130b1a56b405.js"
+  "/react-stopwatch-timer-demo/precache-manifest.be67bd7454124d9818dc194d464879e9.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/react-stopwatch-timer-demo/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
